91b4f2a7fa0c326d3b43f2141bed732e1a023bdd
Ngalim Siregar <ngalim.siregar@gmail.com>
2019-10-22 22:52:58 +0700

    add gino as requirements

--- config/environment/base.py
+++ config/environment/base.py
@@ -2,3 +2,4 @@ class BaseConfig:
     TESTING = False
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/base"
+    BCRYPT_LOG_ROUND = 13
--- config/environment/testing.py
+++ config/environment/testing.py
@@ -4,3 +4,4 @@ from config.environment.base import BaseConfig
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@db:5432/letsgo_testing"
+    BCRYPT_LOG_ROUND = 4
